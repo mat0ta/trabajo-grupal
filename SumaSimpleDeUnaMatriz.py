47ed1cc3f32b7f607cb1e440dbd5acf6a0a30d34
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2021-12-15 19:59:10 +0100

    fin(provisional)

--- SumaSimpleDeUnaMatriz.py
+++ SumaSimpleDeUnaMatriz.py
@@ -14,9 +14,10 @@ def simpleArraySum(ar):
 
 
 if __name__ == '__main__':
-fptr = open(os.environ['OUTPUT_PATH'], 'w')
-ar_count = int(input().strip())
-ar = list(map(int, input().rstrip().split()))
-result = simpleArraySum(ar)
-fptr.write(str(result) + '\n')
-fptr.close()
+    fptr= open('T3-1.txt', 'w')
+    ar_count = int(input().strip())
+    ar = list(map(int, input().rstrip().split()))
+    print(ar)
+    result = simpleArraySum(ar)
+    fptr.write(str(result) + '\n')
+    fptr.close()

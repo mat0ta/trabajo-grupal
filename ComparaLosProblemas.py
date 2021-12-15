ab97eb548aaaf9e7bcf491dad92126f99832635b
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2021-12-15 20:10:05 +0100

    cambio

--- ComparaLosProblemas.py
+++ ComparaLosProblemas.py
@@ -4,7 +4,24 @@ import random
 import re
 import sys
 def compareTriplets(a, b):
+    puntosJugador1=0
+    puntosJugador2=0
+    marcador=[]
     
+    for i,j in zip(a,b):
+        print(i,j)
+        if i>j:
+            puntosJugador1=puntosJugador1+1
+            print('Puntua jugador 1', str(puntosJugador1))
+        elif i<j:
+            puntosJugador2=puntosJugador2+1
+            print('Puntua jugador 2', str(puntosJugador2))
+    marcador.append(puntosJugador1)
+    marcador.append(puntosJugador2)
+    print(marcador)
+    return marcador
+
+
     
 
 if __name__ == '__main__':

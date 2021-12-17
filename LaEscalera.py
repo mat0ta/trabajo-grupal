4957c31f9095c22e41941b2eff62cc7c1b1a0641
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2021-12-17 17:51:23 +0100

    fin(provisional)

--- LaEscalera.py
+++ LaEscalera.py
@@ -5,7 +5,11 @@ import re
 import sys
 
 def staircase(n):
-    
+    for i in range(1,n+1):
+        fila=''
+        for j in range(i):
+            fila += '' .join('# ')
+            print(fila)
 
 
 if __name__ == '__main__':

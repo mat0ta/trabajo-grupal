a9cafcd107dd9e4d9a6d89fb3aa9960c41963987
Matota <72810000+mat0ta@users.noreply.github.com>
2021-12-18 16:10:19 +0100

    Update juego_de_piedras.py

--- juego_de_piedras.py
+++ juego_de_piedras.py
@@ -1,1 +1,20 @@
 import os
+
+def juego(i):
+    ganador = ''
+    if(jugada(i) != 0):
+        ganador = "El jugador 1 es el ganador"
+    else:
+        ganador = "El jugador 2 es el ganador"
+    return ganador
+
+def jugada(i):
+    jugada = 0
+    modulo = i % 7
+    if(modulo >= 2 and modulo <= 3):
+        jugada = 2
+    elif(modulo == 4):
+        jugada = 3
+    elif(modulo >= 5 and modulo <= 6):
+        jugada = 5
+    return jugada

5f47ed7b44f70e1eb4a7aa2ddcff18886157fb3d
Matota <72810000+mat0ta@users.noreply.github.com>
2021-12-18 16:16:37 +0100

    Update calificacion_estudiante.py

--- calificacion_estudiante.py
+++ calificacion_estudiante.py
@@ -18,3 +18,17 @@ def notafinal(notas):
         else:
             redondeo = notas
     return redondeo
+
+
+
+if __name__ == '__main__':
+
+    print("Número de estudiantes")
+    numero_notas = int(input('Introduce las notas de los estudiantes: ').strip())
+    notas = []
+    for _ in range(numero_notas):
+        print("Nota de cada estudiante")
+        notas_dadas = int(input().strip())
+        notas.append(notas_dadas)
+
+    result = calificacion(notas)

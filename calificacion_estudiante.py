c15c0532bddbd7ad209561da2b4a351bc98da116
Matota <72810000+mat0ta@users.noreply.github.com>
2021-12-18 16:15:33 +0100

    Update calificacion_estudiante.py

--- calificacion_estudiante.py
+++ calificacion_estudiante.py
@@ -1,1 +1,20 @@
 import os
+
+def calificacion(notas):
+    list = []
+    for nota in (notas):
+        list.append(notafinal(nota))
+    return list
+
+def notafinal(notas):
+    redondeo = 0
+    if(notas < 40):
+        redondeo = notas
+    else:
+        cociente = int(notas/5 + 1)
+        multiplo = cociente * 5
+        if(multiplo - notas < 3):
+            redondeo = multiplo
+        else:
+            redondeo = notas
+    return redondeo
